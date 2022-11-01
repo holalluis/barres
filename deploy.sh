923917d0624c14bf978b4350fcbb674fd91b7cef
Lluís Bosch <holalluis@gmail.com>
2022-11-01 16:24:42 +0100

    inicial

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#git
+git add *
+git commit -am 'update'
+git push
+
+#desplega al servidor
+# -h  human readable format
+# -P  mostra progrés
+# -vv incrementa verbositat
+# -r  actua recursivament
+rsync --rsync-path="sudo rsync" -hPvvr . debian@51.91.250.220:/var/www/html/barres
